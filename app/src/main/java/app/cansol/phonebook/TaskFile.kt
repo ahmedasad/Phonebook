32be209dfaf2c8f7bdd127d1b2aec08274e8a258
Syed Ahmed <ahmedasad2010@gmail.com>
2020-02-11 12:54:35 +0500

    Repository Created

---
+++ app/src/main/java/app/cansol/phonebook/TaskFile.kt
@@ -0,0 +1,25 @@
+package app.cansol.phonebook
+
+//ANDROID TASK:
+//
+//Phone Book APP
+//
+//Create Phone Book mobile app using kotlin and MVVM pattern. you can use mockapi.io or any other mock api service for apis.
+//
+//You should create a public github/bitbucket/gitlab repo. Make small commits during your development. Submit repo url.
+//
+//Repo must contain readme including how to run your app.
+//
+//Mobile App: Should be developed using kotlin and MVVM pattern (Retrofit would be recommended for network calls)
+//
+//App Features:
+//
+//- User Should be able to login (you can hardcode a user)
+//
+//- User can create contact (name and number)
+//
+//- User can edit contact Item.
+//
+//- User can delete contact Item.
+//
+//- Code should have properly comments.
